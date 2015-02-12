db2911064ca6df9a2a4d2d11880be10b53efe54b
Rowan Walsh <rwn.walsh@gmail.com>
2015-02-12 14:16:37 -0800

    Updated 2-axis math to take floats as input
    2-axis math (geometry.c) now takes in float variables (the positions of
    the shadows along the TSL1401 sensor). The offsets of the TSL's from the
    filament sensor's origin were added as constants to geometry.h .
    
    This allows the 2-axis math to be used with the current sub-pixel
    function.

--- code/sensor/sensor/geometry.c
+++ code/sensor/sensor/geometry.c
@@ -3,14 +3,37 @@
 #include <math.h>
 #include "geometry.h"
 
+/* Adds offset based on TSL1401 sensor placements in 2-axis sensor
+*/
+void GEO_add_sensor_offsets(float edge_position_a[], float edge_position_b[])
+{
+	edge_position_a[0] += GEO_A_SENSOR_OFFSET_MM;
+	edge_position_a[1] += GEO_A_SENSOR_OFFSET_MM;
+	edge_position_b[0] += GEO_B_SENSOR_OFFSET_MM;
+	edge_position_b[1] += GEO_B_SENSOR_OFFSET_MM;
+}
+
+/* Returns an averaged diameter based on the two perpedicular widths
+*/
+inline float GEO_averaged_diameter(float diameter_A, float diameter_B)
+{
+	// Elliptical Mean
+	// return sqrt(diameter_A * diameter_B);
+
+	// Mean
+	return 0.5 * (diameter_A + diameter_B);
+}
+
 /* Returns the filament diameter
 */
-extern float GEO_filament_diameter(int32_t shadow_indice_a[], int32_t shadow_indice_b[])
+extern float GEO_filament_diameter(float edge_position_a[], float edge_position_b[])
 {
-	float x_a1 = ((float)shadow_indice_a[1] - (float)GEO_NM_PER_MM) - GEO_A_LED_Y_MM;
-	float x_a0 = ((float)shadow_indice_a[0] - (float)GEO_NM_PER_MM) - GEO_A_LED_Y_MM;
-    float x_b1 = ((float)shadow_indice_b[1] - (float)GEO_NM_PER_MM) - GEO_B_LED_X_MM;
-    float x_b0 = ((float)shadow_indice_b[0] - (float)GEO_NM_PER_MM) - GEO_B_LED_X_MM;
+	GEO_add_sensor_offsets(edge_position_a, edge_position_b);
+
+	float x_a1 = edge_position_a[1] - GEO_A_LED_Y_MM;
+	float x_a0 = edge_position_a[0] - GEO_A_LED_Y_MM;
+    float x_b1 = edge_position_b[1] - GEO_B_LED_Y_MM;
+    float x_b0 = edge_position_b[0] - GEO_B_LED_X_MM;
 
     float X_a = (x_a1 + x_a0 == 0) ? 0 : GEO_A_LED_X_MM * tan(0.5 * (atan2(x_a1, GEO_A_LED_X_MM) + atan2(x_a0, GEO_A_LED_X_MM)));
     float X_b = (x_b1 + x_b0 == 0) ? 0 : GEO_B_LED_Y_MM * tan(0.5 * (atan2(x_b1, GEO_B_LED_Y_MM) + atan2(x_b0, GEO_B_LED_Y_MM)));
@@ -25,15 +48,4 @@ extern float GEO_filament_diameter(int32_t shadow_indice_a[], int32_t shadow_ind
     float dia_b = 2.0 / 10.0 * sqrt((fil_x - GEO_B_LED_X_MM) * (fil_x - GEO_B_LED_X_MM) + (GEO_B_LED_Y_MM - fil_y) * (GEO_B_LED_Y_MM - fil_y)) * sin(0.5 * (atan2(x_b1, GEO_B_LED_Y_MM) - atan2(x_b0, GEO_B_LED_Y_MM)));
 
 	return GEO_averaged_diameter(dia_a, dia_b);
-}
-
-/* Returns an averaged diameter based on the two perpedicular widths
-*/
-inline float GEO_averaged_diameter(float diameter_A, float diameter_B)
-{
-	// Elliptical Mean
-	// return sqrt(diameter_A * diameter_B);
-
-	// Mean
-	return 0.5 * (diameter_A + diameter_B);
 }
--- code/sensor/sensor/geometry.h
+++ code/sensor/sensor/geometry.h
@@ -4,13 +4,14 @@
 #include <stdint.h>
 
 /* Geometry constants */
-#define GEO_NM_PER_MM (1000000U)
 #define GEO_A_LED_X_MM 150.0
 #define GEO_A_LED_Y_MM 30.0
 #define GEO_B_LED_X_MM 30.0
 #define GEO_B_LED_Y_MM 150.0
+#define GEO_A_SENSOR_OFFSET_MM 10.0
+#define GEO_B_SENSOR_OFFSET_MM 10.0
 
 /* Filament diamter calculations for 2-axis sensor */
-extern float GEO_filament_diameter(int32_t shad_ind_a[], int32_t shad_ind_b[])
+extern float GEO_filament_diameter(float edge_position_a[], float edge_position_b[]);
 
 #endif
