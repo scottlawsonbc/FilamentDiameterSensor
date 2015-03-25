b75f3b96eb60be3bfff896227a2bc8d1a2c3e1f1
Scott Lawson <scottlawsonbc@gmail.com>
2015-03-25 12:08:16 -0700

    Improved debugging output for I2C commands
    Moved uprintf calls to occur after an I2C response has been sent, to
    reduce command response latency when debugging output is enabled. Added
    an error command statement if the command was not recognized by the
    slave. Added COM_AVERAGING_SAMPLES handler to COM_ExecuteCommand

--- code/sensor/sensor/source/i2c-commands.c
+++ code/sensor/sensor/source/i2c-commands.c
@@ -6,6 +6,11 @@ void COM_SetAveraging(uint16_t samples)
 	if (samples > 0)
 	{
 		MAIN_AveragingIterations = samples;
+		uprintf("Averaging samples set to %i\r\n", samples);
+	}
+	else
+	{
+		uprintf("Error: invalid averaging samples: %i\r\n", samples);
 	}
 }
 
@@ -14,24 +19,26 @@ void COM_SetSensorEnabledState(uint8_t state)
 {
 	/* !! forces value to 0 or 1 by double inverting */
 	MAIN_SetSensorState((FunctionalState)!!state);
+	uprintf("Sensor state set to %i\r\n", state);
 }
 
+/* Prints the state of each I2C flag to USART1 to assist in debugging I2C communication */
 void COM_PrintI2CFlags()
 {
-	uprintf("Flag: I2C_FLAG_TXE:%i\r\n",     I2C_GetFlagStatus(I2C, I2C_FLAG_TXE));
-	uprintf("Flag: I2C_FLAG_TXIS:%i\r\n",    I2C_GetFlagStatus(I2C, I2C_FLAG_TXIS));
-	uprintf("Flag: I2C_FLAG_RXNE:%i\r\n",    I2C_GetFlagStatus(I2C, I2C_FLAG_RXNE));
-	uprintf("Flag: I2C_FLAG_ADDR:%i\r\n",    I2C_GetFlagStatus(I2C, I2C_FLAG_ADDR));
-	uprintf("Flag: I2C_FLAG_NACKF:%i\r\n",   I2C_GetFlagStatus(I2C, I2C_FLAG_NACKF));
-	uprintf("Flag: I2C_FLAG_STOPF:%i\r\n",   I2C_GetFlagStatus(I2C, I2C_FLAG_STOPF));
-	uprintf("Flag: I2C_FLAG_TC:%i\r\n",      I2C_GetFlagStatus(I2C, I2C_FLAG_TC));
-	uprintf("Flag: I2C_FLAG_TCR:%i\r\n",     I2C_GetFlagStatus(I2C, I2C_FLAG_TCR));
-	uprintf("Flag: I2C_FLAG_BERR:%i\r\n",    I2C_GetFlagStatus(I2C, I2C_FLAG_BERR));
-	uprintf("Flag: I2C_FLAG_ARLO:%i\r\n",    I2C_GetFlagStatus(I2C, I2C_FLAG_ARLO));
-	uprintf("Flag: I2C_FLAG_OVR:%i\r\n",     I2C_GetFlagStatus(I2C, I2C_FLAG_OVR));
-	uprintf("Flag: I2C_FLAG_PECERR:%i\r\n",  I2C_GetFlagStatus(I2C, I2C_FLAG_PECERR));
-	uprintf("Flag: I2C_FLAG_TIMEOUT:%i\r\n", I2C_GetFlagStatus(I2C, I2C_FLAG_TIMEOUT));
-	uprintf("Flag: I2C_FLAG_ALERT:%i\r\n",   I2C_GetFlagStatus(I2C, I2C_FLAG_ALERT));
+	uprintf("Flag: I2C_FLAG_TXE:%i\r\n",         I2C_GetFlagStatus(I2C, I2C_FLAG_TXE));
+	uprintf("Flag: I2C_FLAG_TXIS:%i\r\n",        I2C_GetFlagStatus(I2C, I2C_FLAG_TXIS));
+	uprintf("Flag: I2C_FLAG_RXNE:%i\r\n",        I2C_GetFlagStatus(I2C, I2C_FLAG_RXNE));
+	uprintf("Flag: I2C_FLAG_ADDR:%i\r\n",        I2C_GetFlagStatus(I2C, I2C_FLAG_ADDR));
+	uprintf("Flag: I2C_FLAG_NACKF:%i\r\n",       I2C_GetFlagStatus(I2C, I2C_FLAG_NACKF));
+	uprintf("Flag: I2C_FLAG_STOPF:%i\r\n",       I2C_GetFlagStatus(I2C, I2C_FLAG_STOPF));
+	uprintf("Flag: I2C_FLAG_TC:%i\r\n",          I2C_GetFlagStatus(I2C, I2C_FLAG_TC));
+	uprintf("Flag: I2C_FLAG_TCR:%i\r\n",         I2C_GetFlagStatus(I2C, I2C_FLAG_TCR));
+	uprintf("Flag: I2C_FLAG_BERR:%i\r\n",        I2C_GetFlagStatus(I2C, I2C_FLAG_BERR));
+	uprintf("Flag: I2C_FLAG_ARLO:%i\r\n",        I2C_GetFlagStatus(I2C, I2C_FLAG_ARLO));
+	uprintf("Flag: I2C_FLAG_OVR:%i\r\n",         I2C_GetFlagStatus(I2C, I2C_FLAG_OVR));
+	uprintf("Flag: I2C_FLAG_PECERR:%i\r\n",      I2C_GetFlagStatus(I2C, I2C_FLAG_PECERR));
+	uprintf("Flag: I2C_FLAG_TIMEOUT:%i\r\n",     I2C_GetFlagStatus(I2C, I2C_FLAG_TIMEOUT));
+	uprintf("Flag: I2C_FLAG_ALERT:%i\r\n",       I2C_GetFlagStatus(I2C, I2C_FLAG_ALERT));
 	uprintf("Flag: I2C_FLAG_BUSY:%i\r\n\r\n",    I2C_GetFlagStatus(I2C, I2C_FLAG_BUSY));
 }
 
@@ -39,24 +46,25 @@ void COM_PrintI2CFlags()
 void COM_SendMeasurement()
 {
 	uint16_t diameter = (uint16_t)(MAIN_FilamentDiameter_MM * 1000.0);
+	uint16_t timeout = I2C_LONG_TIMEOUT;
 
 	/* Clear the ADDR flag */
-	uint16_t timeout = I2C_LONG_TIMEOUT;
-	while(I2C_GetFlagStatus(I2C, I2C_FLAG_ADDR) == RESET)
+	I2C_ClearFlag(I2C, I2C_FLAG_ADDR);
+	COM_PrintI2CFlags();
+	while(I2C_GetFlagStatus(I2C, I2C_FLAG_TXIS) == RESET)
 	{
 		if((timeout--) == 0) return;
 	}
-	I2C_ClearFlag(I2C, I2C_FLAG_ADDR);
+	I2C_ClearFlag(I2C, I2C_FLAG_TXIS);
 	COM_PrintI2CFlags();
-
-	I2C_SendData(I2C, (uint8_t)((diameter & 0xFF00) >> 8));
+	I2C_SendData(I2C, (uint8_t)(diameter >> 8));
 	COM_PrintI2CFlags();
 	timeout = I2C_LONG_TIMEOUT;
 	while(I2C_GetFlagStatus(I2C, I2C_FLAG_TXE) == RESET)
 	{
 		if((timeout--) == 0) return;
 	}
-	I2C_SendData(I2C, (uint8_t)(diameter & 0x00FF));
+	I2C_SendData(I2C, (uint8_t)(diameter));
 
 	/* Wait for not busy */
 	timeout = I2C_LONG_TIMEOUT;
@@ -65,30 +73,34 @@ void COM_SendMeasurement()
 		if((timeout--) == 0) return;
 	}
 	I2C_ClearFlag(I2C, I2C_FLAG_STOPF);
-
+	uprintf("Transmitted\r\n");
 	COM_PrintI2CFlags();
-
-	uprintf("Sent data!\r\n");
-
+	uprintf("Measurement has been transmitted via I2C!\r\n");
 }
 
+/* Executes the specified I2C command */
 void COM_ExecuteCommand(uint8_t command, uint8_t* data, uint8_t numberOfBytes)
 {
 	switch(command)
 	{
 	case COM_ENABLE_SENSOR:
-		uprintf("Enabling sensor!\r\n");
 		COM_SetSensorEnabledState(ENABLE);
+		uprintf("Command: [Enable Sensor] received via I2C\r\n");
 		break;
 	case COM_DISABLE_SENSOR:
-		uprintf("Disabling sensor!\r\n");
 		COM_SetSensorEnabledState(DISABLE);
+		uprintf("Command: [Disable Sensor] received via I2C\r\n");
 		break;
 	case COM_REQUEST_MEASUREMENT:
-		uprintf("Measurement requested!\r\n");
 		COM_SendMeasurement();
+		uprintf("Command: [Request Measurement] received via I2C\r\n");
 		break;
-	default: 
+	case COM_AVERAGING_SAMPLES:
+		COM_SetAveraging(((uint16_t)data[0] << 8) & (uint16_t)data[1]);
+		uprintf("Command: [Averaging Samples] received via I2C\r\n");
+		break;
+	default:
+		uprintf("Unrecognized command: [%x] received via I2C\r\n", command);
 		break;
 	}
 }
--- code/sensor/sensor/source/i2c-commands.h
+++ code/sensor/sensor/source/i2c-commands.h
@@ -7,7 +7,8 @@
 #define COM_ENABLE_SENSOR                   (0x01U)
 #define COM_DISABLE_SENSOR		            (0x02U)
 #define COM_REQUEST_MEASUREMENT             (0x03U)
-#define COM_SET_AVERAGING                   (0x04U)
+#define COM_AVERAGING_SAMPLES               (0x04U)
+
 
 extern void COM_ExecuteCommand(uint8_t command, uint8_t* data, uint8_t numberOfBytes);
 
