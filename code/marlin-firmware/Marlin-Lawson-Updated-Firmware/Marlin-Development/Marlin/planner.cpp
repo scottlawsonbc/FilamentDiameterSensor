48f4cec50fe626b2be72123865291f5f10f54481
Scott Lawson <scottlawsonbc@gmail.com>
2015-03-28 17:48:16 -0700

    removed commented code from marlin

--- code/marlin-firmware/Marlin-Lawson-Updated-Firmware/Marlin-Development/Marlin/planner.cpp
+++ code/marlin-firmware/Marlin-Lawson-Updated-Firmware/Marlin-Development/Marlin/planner.cpp
@@ -601,6 +601,8 @@ void plan_buffer_line(const float &x, const float &y, const float &z, const floa
   block->steps_x = labs((target[X_AXIS] - position[X_AXIS]) + (target[Y_AXIS] - position[Y_AXIS]));
   block->steps_y = labs((target[X_AXIS] - position[X_AXIS]) - (target[Y_AXIS] - position[Y_AXIS]));
 #endif
+  /* ENPH 459 */
+  /* Volumetric multiplier */
   block->steps_z = labs(target[Z_AXIS] - position[Z_AXIS]);
   block->steps_e = labs(target[E_AXIS] - position[E_AXIS]);
   block->steps_e *= volumetric_multiplier[active_extruder];
@@ -804,49 +806,6 @@ void plan_buffer_line(const float &x, const float &y, const float &z, const floa
 
   block->nominal_speed = block->millimeters * inverse_second; // (mm/sec) Always > 0
   block->nominal_rate = ceil(block->step_event_count * inverse_second); // (step/sec) Always > 0
-
-  /*#ifdef FILAMENT_SENSOR
-    //FMM update ring buffer used for delay with filament measurements
-
-
-    if ((extruder == FILAMENT_SENSOR_EXTRUDER_NUM) && (delay_index2 > -1)) //only for extruder with filament sensor and if ring buffer is initialized
-    {
-      delay_dist = delay_dist + delta_mm[E_AXIS];  //increment counter with next move in e axis
-
-      while (delay_dist >= (10 * (MAX_MEASUREMENT_DELAY + 1))) //check if counter is over max buffer size in mm
-        delay_dist = delay_dist - 10 * (MAX_MEASUREMENT_DELAY + 1); //loop around the buffer
-      while (delay_dist < 0)
-        delay_dist = delay_dist + 10 * (MAX_MEASUREMENT_DELAY + 1); //loop around the buffer
-
-      delay_index1 = delay_dist / 10.0; //calculate index
-
-      //ensure the number is within range of the array after converting from floating point
-      if (delay_index1 < 0)
-        delay_index1 = 0;
-      else if (delay_index1 > MAX_MEASUREMENT_DELAY)
-        delay_index1 = MAX_MEASUREMENT_DELAY;
-
-      if (delay_index1 != delay_index2) //moved index
-      {
-        meas_sample = widthFil_to_size_ratio() - 100; //subtract off 100 to reduce magnitude - to store in a signed char
-      }
-      while ( delay_index1 != delay_index2)
-      {
-        delay_index2 = delay_index2 + 1;
-        if (delay_index2 > MAX_MEASUREMENT_DELAY)
-          delay_index2 = delay_index2 - (MAX_MEASUREMENT_DELAY + 1); //loop around buffer when incrementing
-        if (delay_index2 < 0)
-          delay_index2 = 0;
-        else if (delay_index2 > MAX_MEASUREMENT_DELAY)
-          delay_index2 = MAX_MEASUREMENT_DELAY;
-
-        measurement_delay[delay_index2] = meas_sample;
-      }
-
-
-    }
-  #endif
-  */
 
 /* ENPH 459 */
 /* Scott: I think this code updates the circular buffer for filament measurements */
